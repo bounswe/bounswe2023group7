505d1832b78ad09a50d712619d7a59d30d7c7ebf
omersafakbebek <omersafakbebek@gmail.com>
2023-05-06 10:24:23 +0300

    added db connection and dotenv module

--- practice-app/src/app.js
+++ practice-app/src/app.js
@@ -1,13 +1,15 @@
 import express from "express";
 import morgan from "morgan";
 import router from "./routes/index.js"
+import { config } from "dotenv";
+import connectToDb from "./utils/db.js";
 const app = express();
-
+config();
 app.use( morgan('dev'));
 app.use("/api", router );
-const port = 8080;
-
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
   console.log(`App started at http://localhost:${port}`);
+  connectToDb();
 });
