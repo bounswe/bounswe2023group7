5211ee80d9e0e7c16ee54b1bead6b4959059ed89
omersafakbebek <omersafakbebek@gmail.com>
2023-05-06 12:10:24 +0300

    added swagger

--- practice-app/src/app.js
+++ practice-app/src/app.js
@@ -1,13 +1,17 @@
 import express from "express";
 import morgan from "morgan";
-import router from "./routes/index.js"
+import router from "./routes/index.js";
+import swaggerDocs from "./utils/swagger.js";
+import { config } from "dotenv";
+config();
 const app = express();
 
-app.use( morgan('dev'));
+app.use(morgan('dev'));
 app.use("/api", router );
 const port = 8080;
-
+const apiUrl = process.env.API_URL;
 
 app.listen(port, () => {
-  console.log(`App started at http://localhost:${port}`);
+  console.log(`App started at http://${apiUrl}`);
+  swaggerDocs(app, apiUrl);
 });
--- practice-app/src/routes/index.js
+++ practice-app/src/routes/index.js
@@ -1,5 +1,17 @@
 import express from "express";
 const router = express.Router();
+/**
+ * @openapi
+ * '/api':
+ *  get:
+ *      tags:
+ *      - API
+ *      summary: Welcome Info
+ *      responses:
+ *          200:
+ *              description: Success
+ */
+
 router.get('/', (_, res) => {
     return res.status(200).send("Welcome to the practice app!");
 });
