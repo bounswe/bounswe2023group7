699eacf369767be9d02093da2da2b5cce6da46e1
Elif Kizilkaya <69367633+elifkizilky@users.noreply.github.com>
2023-05-09 13:27:44 +0000

    added location route to index file

--- practice-app/src/routes/index.js
+++ practice-app/src/routes/index.js
@@ -1,5 +1,9 @@
 import express from "express";
 const router = express.Router();
+
+import locationRouter from './location.routes.js'
+router.use("/location", locationRouter);
+
 /**
  * @openapi
  * '/api':
@@ -15,5 +19,7 @@ const router = express.Router();
 router.get('/', (_, res) => {
     return res.status(200).send("Welcome to the practice app!");
 });
+
+
 
 export default router;
