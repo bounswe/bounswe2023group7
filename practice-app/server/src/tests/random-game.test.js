015155d0f6120e7507d0e365d703a5276ebde28e
M. Tayyip Kamiloglu <mt.kamiloglu@gmail.com>
2023-05-12 15:23:15 +0300

    wrote lacking test functions

--- practice-app/server/src/tests/random-game.test.js
+++ practice-app/server/src/tests/random-game.test.js
@@ -1,14 +1,55 @@
 import request from "supertest";
 import app from "../app.js";
 
+let accessToken = '';
 describe("Random Game Test", () => {
-    test("GET /api/random-game", (done) => {
-        request(app)
-        .get("/api/random-game")
-        .expect(200)
-        .end((err, res) => {
-            if (err) return done(err);
-            return done();
+    beforeAll(async () => {
+        try {
+            await request(app).post("/api/users/signup").send({ username: "test", email: "test@email.com", password: "testpassword" });
+            const response = await request(app).post("/api/users/login").send({ identifier: "test", password: "testpassword" });
+            accessToken = response.body.accessToken;
+        } catch (error) {
+        }
+    });
+    afterAll(async () => {
+        try {
+            await User.deleteOne({ username: "test" });
+        } catch (error) {
+        }
+    });
+
+    describe("Should return 200", () => {
+        test("GET /api/random-game/history", (done) => {
+            request(app)
+                .get("/api/random-game/history")
+                .set('Authorization', accessToken)
+                .expect(200)
+                .then()
+        }, 30000);
+    });
+    describe("Should return 401", () => {
+        test("GET /api/random-game/history", (done) => {
+            request(app)
+            .get("/api/random-game/history")
+            .expect(401)
+            .then()
         });
     });
+    describe("Should return 200", () => {
+        test("POST /api/random-game", (done) => {
+            request(app)
+            .get("/api/random-game")
+            .set('Authorization', accessToken)
+            .expect(200)
+            .then()
+        }, 30000);
+    });describe("Should return 401", () => {
+        test("POST /api/random-game", (done) => {
+            request(app)
+            .get("/api/random-game/")
+            .expect(401)
+            .then()
+        }, 30000);
+    });
+
 });
