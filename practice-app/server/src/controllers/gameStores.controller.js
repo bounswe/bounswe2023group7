0cc2e2fcf1f20341ffbb8e0c1ad99483b702af48
Melih Gezer <melih_gezer211@hotmail.com>
2023-05-12 15:40:39 +0300

    finished the fronted and backend part

--- practice-app/server/src/controllers/gameStores.controller.js
+++ practice-app/server/src/controllers/gameStores.controller.js
@@ -74,34 +74,26 @@ class GameStoresController {
     }
 
     async addGametoCart(req, res) {
-
         try {
             const email = req.email;
-
             if(!email){
                 return res.status(400).json({ message: "Please provide the required fields."});
             }
-
             const gameName = req.body.name;
 
             if(!(gameName)){
                 return res.status(400).json({ message: "Pleasee provide the required fields."});
             }
-
             const priceURL = baseURL + `games?title=${gameName}&limit=1`;
             const [resp] = (await axios.get(priceURL)).data;
 
             if(!resp){
                 return res.status(400).json({ message: "Game not found."});
             }
-
             const dealID = resp.cheapestDealID;
-
             const gameInfoURL = baseURL + `deals?id=${dealID}`;
-
             const response = (await axios.get(gameInfoURL)).data;
-            
-
+        
             const shortResponse = {
                 "email" : email,
                 "game_name": response.gameInfo.name,
@@ -110,7 +102,6 @@ class GameStoresController {
                 "retail_price": response.gameInfo.retailPrice,
                 "img_url": response.gameInfo.thumb
             }
-
             const gameInCart = await GamesCart.find({ "email": email, "game_name" : response.gameInfo.name});
 
             if (gameInCart.length != 0) {
@@ -120,9 +111,7 @@ class GameStoresController {
                 const game = new GamesCart(shortResponse);
                 game.save();
                 res.status(201).send(shortResponse);
-            }
-
-            
+            }  
         }
         catch(e) {
             res.status(500).send(e)
