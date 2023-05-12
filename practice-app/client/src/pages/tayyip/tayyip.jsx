62952182bcb5a51666689790d1ec76a6c290f767
M. Tayyip Kamiloglu <mt.kamiloglu@gmail.com>
2023-05-12 15:56:52 +0300

    cosmetic changes

--- practice-app/client/src/pages/tayyip/tayyip.jsx
+++ practice-app/client/src/pages/tayyip/tayyip.jsx
@@ -2,20 +2,20 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./tayyip-style.css"
 import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import History from "./randomGameHistory.jsx";
 import Box from '@mui/material/Box';
-import { Link } from 'react-router-dom';
 import Typography from '@mui/material/Typography';
-import { useNavigate } from 'react-router-dom';
+import CircularProgress from '@mui/material/CircularProgress';
+import { Link } from 'react-router-dom';
 
 const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Im10ayIsImlhdCI6MTY4MzczMzE3N30.CijpcqEFWUs-5DrKe01WefwSOulYnWfn3qP22UU2XXw";
 
 function App() {
   const [game, setGame] = useState({});
   const [games, setGames] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   async function handleSubmit() {
+    setIsLoading(true);
     try {
       const result = await axios.post('http://localhost:8080/api/random-game',
         {},
@@ -28,6 +28,7 @@ function App() {
     } catch (error) {
       console.error(error);
     }
+    setIsLoading(false);
   };
 
 
@@ -46,25 +47,6 @@ function App() {
 
   return (
     <div className="App">
-      <Box sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        minHeight: '55vh',
-        padding: '2rem',
-        color: 'dark grey',
-      }}>
-        {game.name && (
-          <div className="game-card">
-            <img src={game.image} alt={game.name} />
-            <div className="game-info">
-              <h2>{game.name}</h2>
-              <p>{game.short_description}</p>
-            </div>
-          </div>
-        )}
-      </Box>
       <div className="button-container">
         <Button onClick={() => handleSubmit()}
           variant="contained"
@@ -76,6 +58,33 @@ function App() {
           to="/random-game-history"
           sx={{ backgroundColor: '#424242', color: '#FFFFFF', minWidth: '200px', fontFamily: "Trebuchet MS", alignItems: "center" }}>Get History</Button>
       </div>
+      <Box sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        minHeight: '55vh',
+        padding: '2rem',
+        color: 'dark grey',
+      }}>
+        {isLoading ? (
+          <div className="loading-animation">
+            <CircularProgress />
+          </div>
+        ) : game.name ? (
+          <div className="game-card">
+            <img src={game.image} alt={game.name} />
+            <div className="game-info">
+              <h2>{game.name}</h2>
+              <p>{game.short_description}</p>
+            </div>
+          </div>
+        ) : (
+          <Typography variant="h6">
+            Click the "Get Random Game" button to generate a random game.
+          </Typography>
+        )}
+      </Box>
       {games.map((game) => (
         <div className="game-card-container">
           <div className="game-card" key={game.id}>
