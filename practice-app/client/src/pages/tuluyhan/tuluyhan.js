9c25b7ea03d8f90385b73ef9e2130f6a49195dcf
Mehmet Tuluyhan Sözen <tuluy05@gmail.com>
2023-05-12 20:39:14 +0300

    frontend optimize commit

--- practice-app/client/src/pages/tuluyhan/tuluyhan.js
+++ practice-app/client/src/pages/tuluyhan/tuluyhan.js
@@ -1,94 +1,116 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 
-function StreamingGames() {
+import "./style.css";
+
+export default function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [responseMessage, setResponseMessage] = useState("");
   const [mostViewedGame, setMostViewedGame] = useState({});
   const [savedGames, setSavedGames] = useState([]);
-  const [responseMessage, setResponseMessage] = useState('');
-
-  const TWITCH_API_CLIENT_ID = 'wbmzp6h6688riuedz47eezgn1fktgr';
-  const TWITCH_API_TOKEN = 'x6c56s3fpxfgmfzfw144i34297li2k';
 
   const handleGetMostViewedGame = async () => {
+    setIsLoading(true);
+
     try {
-      const response = await axios.get(`http://localhost:8080/api/streaming-games/most-viewed`, {
-        headers: {
-          'Client-ID': TWITCH_API_CLIENT_ID,
-          'Authorization': `Bearer ${TWITCH_API_TOKEN}`
-        }
-      });
+      const response = await axios.get("http://localhost:8080/api/streaming-games/most-viewed");
 
       setMostViewedGame(response.data);
-      setResponseMessage(`Most viewed game retrieved successfully: ${response.data.name} - ${response.data.id}`);
+      setResponseMessage("");
     } catch (error) {
-      console.error(error);
-      setResponseMessage(`Error retrieving most viewed game: ${error.message}`);
+      setMostViewedGame({});
+      setResponseMessage(error.message);
     }
+
+    setIsLoading(false);
   };
 
   const handleSaveMostViewedGame = async () => {
-    try {
-      const response = await axios.post(`http://localhost:8080/api/streaming-games/most-viewed`, null, {
-        headers: {
-          'Client-ID': TWITCH_API_CLIENT_ID,
-          'Authorization': `Bearer ${TWITCH_API_TOKEN}`
-        }
-      });
+    setIsLoading(true);
 
-      setSavedGames([...savedGames, response.data]);
-      setResponseMessage(`Most viewed game saved successfully: ${response.data.name} - ${response.data.id}`);
+    try {
+      await axios.post("http://localhost:8080/api/streaming-games/most-viewed", mostViewedGame);
+
+      setResponseMessage("Most viewed game saved successfully!");
     } catch (error) {
-      console.error(error);
-      setResponseMessage(`Error saving most viewed game: ${error.message}`);
+      setResponseMessage(error.message);
     }
+
+    setIsLoading(false);
   };
 
   const handleGetSavedGames = async () => {
+    setIsLoading(true);
+
     try {
-      const response = await axios.get(`http://localhost:8080/api/streaming-games/history-most-viewed`);
+      const response = await axios.get("http://localhost:8080/api/streaming-games/history-most-viewed");
 
       setSavedGames(response.data);
-      setResponseMessage(`Saved games retrieved successfully.`);
+      setResponseMessage("");
     } catch (error) {
-      console.error(error);
-      setResponseMessage(`Error retrieving saved games: ${error.message}`);
+      setSavedGames([]);
+      setResponseMessage(error.message);
     }
+
+    setIsLoading(false);
   };
 
   return (
-    <div>
-      <button onClick={handleGetMostViewedGame}>Get Most Viewed Game</button>
-
-      <button onClick={handleSaveMostViewedGame}>Save Most Viewed Game</button>
-
-      <button onClick={handleGetSavedGames}>Get Saved Games</button>
-
-      {responseMessage && <p>{responseMessage}</p>}
-
-      {mostViewedGame.name && (
-        <div>
-          <h2>Most Viewed Game</h2>
-          <p>Name: {mostViewedGame.name}</p>
-          <p>ID: {mostViewedGame.id}</p>
-        </div>
-      )}
-
-      {savedGames.length > 0 && (
-        <div>
-          <h2>Saved Games</h2>
-          <ul>
-            {savedGames.map(game => (
-              <li key={game._id}>
-                <p>Name: {game.name}</p>
-                <p>ID: {game.id}</p>
-                <p>Date: {game.date}</p>
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
+    <div className="container">
+      <div className="buttons-container">
+        <button 
+          className="button" 
+          onClick={handleGetMostViewedGame}
+        >
+          Get Most Viewed Game
+        </button>
+  
+        <button 
+          className="button" 
+          onClick={handleSaveMostViewedGame}
+        >
+          Save Most Viewed Game
+        </button>
+  
+        <button 
+          className="button" 
+          onClick={handleGetSavedGames}
+        >
+          Get Saved Games
+        </button>
+      </div>
+  
+      {responseMessage && <p className="response-message">{responseMessage}</p>}
+  
+      <div className="game-info-container">
+        {mostViewedGame.name && (
+          <div className="game-info">
+            <h2>Most Viewed Game</h2>
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              <div className="saved-game-item">
+                <p>Name: {mostViewedGame.name}</p>
+                <p>ID: {mostViewedGame.id}</p>
+              </div>
+            </div>
+          </div>
+        )}
+  
+        {savedGames.length > 0 && (
+          <div className="saved-games">
+            <h2>Saved Games</h2>
+            <div className="saved-games-list">
+              {savedGames.map(game => (
+                <div className="saved-game-item" key={game._id}>
+                  <p>Name: {game.name}</p>
+                  <p>ID: {game.id}</p>
+                  <p>Date: {game.date}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
+  
 }
-
-export default StreamingGames;
