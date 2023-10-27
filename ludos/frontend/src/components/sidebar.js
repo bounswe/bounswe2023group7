37deb3e12c42caf80146cefc0069ab5ea95653ed
Elif Kizilkaya <69367633+elifkizilky@users.noreply.github.com>
2023-10-27 19:20:26 +0000

    Added sidebar according to design #298

--- ludos/frontend/src/components/sidebar.js
+++ ludos/frontend/src/components/sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import List from '@material-ui/core/List';
@@ -9,9 +9,13 @@ import HomeIcon from '@mui/icons-material/Home';
 import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
 import GroupIcon from '@mui/icons-material/Groups';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import IconButton from '@material-ui/core/IconButton';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 import { Link } from 'react-router-dom';
 
-const drawerWidth = 240;
+const drawerWidth = 100; // Adjust the width as needed
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -24,15 +28,39 @@ const useStyles = makeStyles((theme) => ({
   drawerPaper: {
     width: drawerWidth,
     backgroundColor: '#F49A32',
+    display: 'flex',
+    flexDirection: 'column', // Display items in a column
+    alignItems: 'center', // Center items horizontally
+    paddingTop: theme.spacing(0), // Add top padding
   },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
+  listItem: {
+    display: 'flex',
+    flexDirection: 'column', // Display icon and text in a column
+    alignItems: 'center', // Center icon and text horizontally
+    padding: theme.spacing(1),
+  },
+  circleIcon: {
+    backgroundColor: 'rgba(255, 255, 255, 0.6)',
+    borderRadius: '50%',
+    padding: theme.spacing(1.5),
+    marginBottom: theme.spacing(1),
+  },
+  smallText: {
+    fontSize: '6px', // Adjust the font size as needed
   },
 }));
 
 function Sidebar() {
   const classes = useStyles();
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleProfileClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleProfileClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <div className={classes.root}>
@@ -44,31 +72,92 @@ function Sidebar() {
         }}
       >
         <List>
-          <ListItem button component={Link} to="/home">
-            <ListItemIcon>
-              <HomeIcon />
-            </ListItemIcon>
-            <ListItemText primary="Home" />
+          <ListItem button component={Link} to="/">
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+              <ListItemIcon className={classes.listItem} style={{ textAlign: 'center' }}>
+                <div className={classes.circleIcon}>
+                  <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                    <HomeIcon />
+                  </div>
+                </div>
+                <ListItemText primary={<span style={{ fontSize: '15px', textAlign: 'center', fontFamily: 'OCR A Std, monospace', fontWeight: 'bold' }}>Home</span>} />
+              </ListItemIcon>
+            </div>
           </ListItem>
           <ListItem button component={Link} to="/games">
-            <ListItemIcon>
-              <SportsEsportsIcon />
-            </ListItemIcon>
-            <ListItemText primary="Games" />
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+              <ListItemIcon className={classes.listItem} style={{ textAlign: 'center' }}>
+                <div className={classes.circleIcon}>
+                  <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                    <SportsEsportsIcon />
+                  </div>
+                </div>
+                <ListItemText primary={<span style={{ fontSize: '15px', textAlign: 'center', fontFamily: 'OCR A Std, monospace', fontWeight: 'bold' }}>Games</span>} />
+              </ListItemIcon>
+            </div>
           </ListItem>
           <ListItem button component={Link} to="/groups">
-            <ListItemIcon>
-              <GroupIcon />
-            </ListItemIcon>
-            <ListItemText primary="Groups" />
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+              <ListItemIcon className={classes.listItem} style={{ textAlign: 'center' }}>
+                <div className={classes.circleIcon}>
+                  <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                    <GroupIcon />
+                  </div>
+                </div>
+                <ListItemText primary={<span style={{ fontSize: '15px', textAlign: 'center', fontFamily: 'OCR A Std, monospace', fontWeight: 'bold' }}>Groups</span>} />
+              </ListItemIcon>
+            </div>
           </ListItem>
           <ListItem button component={Link} to="/create-game">
-            <ListItemIcon>
-              <AddCircleIcon />
-            </ListItemIcon>
-            <ListItemText primary="Create Game" />
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+              <ListItemIcon className={classes.listItem} style={{ textAlign: 'center' }}>
+                <div className={classes.circleIcon}>
+                  <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                    <AddCircleIcon />
+                  </div>
+                </div>
+                <ListItemText primary={<span style={{ fontSize: '15px', textAlign: 'center', fontFamily: 'OCR A Std, monospace', fontWeight: 'bold' }}>Create Game</span>} />
+              </ListItemIcon>
+            </div>
           </ListItem>
         </List>
+
+        {/* Profile Button */}
+      
+        <IconButton
+          onClick={handleProfileClick}
+          style={{
+            marginTop: 'auto',
+            marginBottom: '10px',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            backgroundColor: 'rgba(255, 255, 255, 0.6)',
+            borderRadius: '50%',
+            width: '3rem', // Set the desired width
+            height: '3rem', // Set the desired height
+          }}
+        >
+          <AccountCircleIcon />
+        </IconButton>
+      
+        {/* Profile Menu */}
+        <Menu
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={handleProfileClose}
+        >
+          <MenuItem component={Link} to="/profile-page" onClick={handleProfileClose}>
+            Profile Page
+          </MenuItem>
+          <MenuItem component={Link} to="/change-password" onClick={handleProfileClose}>
+            Change Password
+          </MenuItem>
+          <MenuItem component={Link} to="/log-out" onClick={handleProfileClose}>
+            Log Out
+          </MenuItem>
+
+        </Menu>
       </Drawer>
       <main className={classes.content}>
         {/* Your main content goes here */}
