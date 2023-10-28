f5aa437d0b89cde546b6a19c7c06c3e8040c2568
M. Tayyip Kamiloglu <mt.kamiloglu@gmail.com>
2023-10-28 13:30:26 +0300

    updated signup page with modern look

--- ludos/frontend/src/components/SignUpForm.js
+++ ludos/frontend/src/components/SignUpForm.js
@@ -1,112 +1,122 @@
-import { React, useState } from "react";
-import { Button, TextField, Typography, Container, Grid, Dialog, DialogContent, DialogContentText, DialogTitle, DialogActions } from '@material-ui/core';
-import axios from "axios";
+import * as React from 'react';
+import { useState } from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import axios from 'axios';
 
+const defaultTheme = createTheme();
 
-export default function SignUp() {
-
-    const [open, setOpen] = useState(false);
-    const [username, setUsername] = useState('');
+export default function SignUpForm() {
     const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
     const axiosInstance = axios.create({
-        baseURL: 'http://3.125.225.39:8080', // Replace with your server's base URL
-    });
+        baseURL: 'http://3.125.225.39:8080'
+    })
 
-    function handleSignUp() {
-
-        axiosInstance.post('/user', { username, email, password })
+    const handleSignup = (event) => {
+        event.preventDefault();
+        axiosInstance.post('/user', { email, username, password })
             .then((response) => {
-                setOpen(true);
+                const token = response.data.token;
             })
             .catch((error) => {
-                console.error('Sign-up error: ', error);
+                console.error('Login error: ', error);
             });
-
-    }
-
-    const handleClose = () => {
-        setOpen(false);
     };
 
     return (
-        <Container>
-            <Grid container spacing={4} style={{
-                position: 'absolute', left: '50%', top: '50%',
-                transform: 'translate(-50%, -50%)',
-                marginLeft: '20px',
-                marginRight: '20px'
-            }}>
-                <Grid item xs={12} md={6} alignItems="center">
-                    <img
-                        src="/logo.png"
-                        alt="Gamer"
-                        style={{ width: '100%', maxHeight: '400px', objectFit: 'contain' }}
-                    />
-                </Grid>
-                <Grid item xs={12} md={6} container spacing={2} justifyContent="center" direction="column">
-                    <Typography variant="h4" align="center">Login</Typography>
-
-                    <form>
-                        <Grid container spacing={2} direction="column" justifyContent="space-evenly">
-                            <Grid item xs={12} md={9}>
-                                <TextField
-                                    label="Username"
-                                    variant="outlined"
-                                    fullWidth
-                                    value={username}
-                                    onChange={(e) => setUsername(e.target.value)}
-                                />
+        <ThemeProvider theme={defaultTheme}>
+            <Grid container component="main" sx={{ height: '100vh' }}>
+                <CssBaseline />
+                <Grid
+                    item
+                    xs={false}
+                    sm={4}
+                    md={7}
+                    sx={{
+                        backgroundImage: 'url()',
+                        backgroundRepeat: 'no-repeat',
+                        backgroundColor: (t) =>
+                            t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center',
+                    }}
+                />
+                <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+                    <Box
+                        sx={{
+                            my: 8,
+                            mx: 4,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <Avatar sx={{ m: 1, bgcolor: '#569CB1' }}>
+                        </Avatar>
+                        <Typography component="h1" variant="h5">
+                            Sign Up
+                        </Typography>
+                        <Box component="form" noValidate sx={{ mt: 1 }}>
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                label="Email Address"
+                                autoComplete="email"
+                                autoFocus
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                            <TextField
+                                margin='normal'
+                                required
+                                fullWidth
+                                label="Username"
+                                variant="outlined"
+                                value={username}
+                                onChange={(e) => setUsername(e.target.value)}
+                            />
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                label="Password"
+                                type="password"
+                                value={password}
+                                autoComplete="current-password"
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                            <Button
+                                type="button"
+                                fullWidth
+                                variant="contained"
+                                sx={{ mt: 3, mb: 2 }}
+                                onClick={handleSignup}
+                            >
+                                Sign Up
+                            </Button>
+                            <Grid container>
+                                <Grid item xs>
+                                    <Link href="#" variant="body2">
+                                        Forgot password?
+                                    </Link>
+                                </Grid>
                             </Grid>
-                            <Grid item xs={12} md={9}>
-                                <TextField
-                                    label="Email"
-                                    variant="outlined"
-                                    fullWidth
-                                    value={email}
-                                    onChange={(e) => setEmail(e.target.value)}
-                                />
-                            </Grid>
-                            <Grid item xs={12} md={9}>
-                                <TextField
-                                    label="Password"
-                                    type="password"
-                                    variant="outlined"
-                                    fullWidth
-                                    value={password}
-                                    onChange={(e) => setPassword(e.target.value)}
-                                />
-                            </Grid>
-                            <Grid item xs={12} md={9}>
-                                <Button
-                                    variant="contained"
-                                    color="primary"
-                                    fullWidth
-                                    onClick={handleSignUp}
-                                >
-                                    Sign Up
-                                </Button>
-                            </Grid>
-                        </Grid>
-                    </form>
+                        </Box>
+                    </Box>
                 </Grid>
             </Grid>
-
-            {/* Başarılı Sign-up Bildirimi */}
-            <Dialog open={open} onClose={handleClose}>
-                <DialogTitle>Success!</DialogTitle>
-                <DialogContent>
-                    <DialogContentText>
-                        Congratulations! Sign-up was successful.
-                    </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClose} color="primary">
-                        OK
-                    </Button>
-                </DialogActions>
-            </Dialog>
-        </Container>
+        </ThemeProvider>
     );
 }
