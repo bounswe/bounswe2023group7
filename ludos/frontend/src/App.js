6f885da03d4dc03a3e1a78230b35d58cf60cf1d6
M. Tayyip Kamiloglu <mt.kamiloglu@gmail.com>
2023-10-29 12:31:01 +0300

    changed App.js

--- ludos/frontend/src/App.js
+++ ludos/frontend/src/App.js
@@ -1,16 +1,20 @@
 
 import SignUpForm from './components/SignUpForm';
 import Login from './components/Login';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/signup" element={<SignUpForm />} />
+        <Route path="/signup" element={
+          <Layout>
+            <SignUpForm />
+          </Layout>
+        }/>
         <Route path="/login" element={<Login />} />
-      </Routes>
+      </Routes>ç
     </Router>
   );
 }
