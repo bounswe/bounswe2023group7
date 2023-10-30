5b256b5a682118c964e87eb649347db4d50ce268
frknlke <furkan.ulke@boun.edu.tr>
2023-10-30 19:08:03 +0300

    #312 updated background colors, font sizes etc.

--- ludos/mobile/lib/activation_for_password_reset.dart
+++ ludos/mobile/lib/activation_for_password_reset.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'helper/colors.dart';
 import 'reset_password.dart';
 
 class EnterActivation extends StatefulWidget {
@@ -14,10 +15,12 @@ class _EnterActivationState extends State<EnterActivation> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFF6b415e),
+      backgroundColor: const Color(0xFF101c2c),
       appBar: AppBar(
-        backgroundColor: const Color(0xFF5f1a37),
-        title: const Text('Forgot Password'),
+        backgroundColor: const Color(0xFFf89c34),
+        title: const Text(
+            'Forgot Password',
+        ),
       ),
       body: SingleChildScrollView(
         child: Padding(
@@ -26,16 +29,18 @@ class _EnterActivationState extends State<EnterActivation> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 //crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: <Widget>[
+                  const SizedBox(height: 20),
                   const Text(
                     'Please enter the activation code sent to your email address.',
                     style: TextStyle(
-                      color: Colors.white60,
+                      color: MyColors.red,
                       fontWeight: FontWeight.bold,
                       fontSize: 16,
                     ),
                   ),
                   const SizedBox(height: 20),
                   TextField(
+                    style: const TextStyle(color: MyColors.white),
                     onChanged: (value) {
                       setState(() {
                         activationCode = value;
@@ -45,30 +50,34 @@ class _EnterActivationState extends State<EnterActivation> {
                     decoration: const InputDecoration(
                       labelText: 'Activation Code',
                       labelStyle: TextStyle(
-                          fontStyle: FontStyle.italic,
-                          fontSize: 15,
-                          color: Color.fromARGB(255, 219, 184, 199),
-                          fontWeight: FontWeight.bold),
+                          color: MyColors.lightBlue, fontWeight: FontWeight.bold),
+                      prefixIcon: Icon(Icons.code),
+                      prefixIconColor: MyColors.lightBlue,
+                      border: UnderlineInputBorder(
+                          borderSide:
+                          BorderSide(color: MyColors.lightBlue, width: 2.0)),
                       focusedBorder: UnderlineInputBorder(
                         borderSide:
-                            BorderSide(color: Color(0xFF5f1a37), width: 2.0),
+                        BorderSide(color: MyColors.lightBlue, width: 2.0),
                       ),
                     ),
-                    cursorColor: const Color(0xFF5f1a37),
+                    cursorColor: MyColors.lightBlue,
                   ),
                   const SizedBox(height: 25),
                   Text(
                    responseForActivationCode,
                       style: const TextStyle(
-                        color: Colors.white60,
+                        color: MyColors.red,
                         fontWeight: FontWeight.bold,
                         fontSize: 16,
                       ),
                   ),
-                  const SizedBox(height: 20),
+                  const SizedBox(height: 40),
                   ElevatedButton(
                     style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xFF5f1a37)),
+                        backgroundColor: const Color(0xFFf89c34),
+                        shape: const StadiumBorder()
+                    ),
                     onPressed: () {
                       // logic to compare activation code with the one sent to the email address
                       // also responseForActivationCode should be updated accordingly
@@ -78,16 +87,22 @@ class _EnterActivationState extends State<EnterActivation> {
                           builder: (context) => (const ResetPassword())));
 
                     },
-                    child: const Text('Reset Password'),
+                    child: const Text(
+                        'Reset Password',
+                      style: TextStyle(color: MyColors.darkBlue),
+                      ),
                   ),
                   const SizedBox(height: 20),
                   ElevatedButton(
                     style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xFF5f1a37)),
+                        backgroundColor: const Color(0xFFf89c34),
+                        shape: const StadiumBorder()
+                    ),
                     onPressed: () {
                       // logic to resend activation code to email address
                     },
-                    child: const Text('Resend Verification Code'),
+                    child: const Text('Resend Verification Code',
+                        style: TextStyle(color: MyColors.darkBlue),),
                   ),
                 ],
               ),
