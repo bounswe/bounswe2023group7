a2dab645401ee3958786e0043ae0ea302fa421ca
omersafakbebek <omersafakbebek@gmail.com>
2023-10-31 01:13:26 +0300

    fixed lint errors

--- ludos/backend/test/game.controller.spec.ts
+++ ludos/backend/test/game.controller.spec.ts
@@ -1,19 +1,13 @@
-import { ConflictException, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import { ConflictException, NotFoundException } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { JwtModule } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
 import { DataSource } from 'typeorm';
-import { UserController } from '../src/controllers/user.controller';
-import { LoginDto } from '../src/dtos/user/request/login.dto';
-import { RegisterDto } from '../src/dtos/user/request/register.dto';
-import { RegisterResponseDto } from '../src/dtos/user/response/register-response.dto';
-import { User } from '../src/entities/user.entity';
-import { UserRepository } from '../src/repositories/user.repository';
-import { UserService } from '../src/services/user.service';
 import { GameController } from '../src/controllers/game.controller';
-import { GameRepository } from '../src/repositories/game.repository';
-import { GameService } from '../src/services/game.service';
 import { Game } from '../src/entities/game.entity';
+import { User } from '../src/entities/user.entity';
+import { GameRepository } from '../src/repositories/game.repository';
+import { UserRepository } from '../src/repositories/user.repository';
+import { GameService } from '../src/services/game.service';
 describe('GameController', () => {
   let gameController: GameController;
   let gameRepository: GameRepository;
@@ -44,19 +38,18 @@ describe('GameController', () => {
     gameController = app.get<GameController>(GameController);
     gameRepository = app.get<GameRepository>(GameRepository);
     userRepository = app.get<UserRepository>(UserRepository);
-
   });
 
   describe('follow', () => {
     it('should return 200', async () => {
       const game = new Game();
       game.followerList = [];
-      game.id = "1";
+      game.id = '1';
       const user = new User();
-      user.id = "1";
+      user.id = '1';
       const req = {
-        user: user
-      }; 
+        user: user,
+      };
       const gameSpy = jest
         .spyOn(gameRepository, 'findGameByIdWithFollowerList')
         .mockResolvedValue(game);
@@ -77,37 +70,39 @@ describe('GameController', () => {
     it('should throw Conflict Exception', async () => {
       const game = new Game();
       game.followerList = [];
-      game.id = "1";
+      game.id = '1';
       const user = new User();
-      user.id = "1";
+      user.id = '1';
       const req = {
-        user: user
-      }; 
+        user: user,
+      };
       game.followerList.push(user);
       const gameSpy = jest
         .spyOn(gameRepository, 'findGameByIdWithFollowerList')
         .mockResolvedValue(game);
-      await expect(gameController.followGame(req as any, game.id)).rejects.toStrictEqual(
-        new ConflictException("The game is followed already!")
+      await expect(
+        gameController.followGame(req as any, game.id),
+      ).rejects.toStrictEqual(
+        new ConflictException('The game is followed already!'),
       );
       expect(gameSpy).toHaveBeenCalledWith(game.id);
     });
     it('should throw Not Found Exception', async () => {
       const game = new Game();
       game.followerList = [];
-      game.id = "1";
+      game.id = '1';
       const user = new User();
-      user.id = "1";
+      user.id = '1';
       const req = {
-        user: user
-      }; 
+        user: user,
+      };
       game.followerList.push(user);
       const gameSpy = jest
         .spyOn(gameRepository, 'findGameByIdWithFollowerList')
         .mockResolvedValue(undefined);
-      await expect(gameController.followGame(req as any, game.id)).rejects.toStrictEqual(
-        new NotFoundException("Game Not Found!")
-      );
+      await expect(
+        gameController.followGame(req as any, game.id),
+      ).rejects.toStrictEqual(new NotFoundException('Game Not Found!'));
       expect(gameSpy).toHaveBeenCalledWith(game.id);
     });
   });
@@ -115,12 +110,12 @@ describe('GameController', () => {
     it('should return 200', async () => {
       const game = new Game();
       game.followerList = [];
-      game.id = "1";
+      game.id = '1';
       const user = new User();
-      user.id = "1";
+      user.id = '1';
       const req = {
-        user: user
-      }; 
+        user: user,
+      };
       game.followerList.push(user);
       const gameSpy = jest
         .spyOn(gameRepository, 'findGameByIdWithFollowerList')
@@ -137,37 +132,38 @@ describe('GameController', () => {
     it('should throw Conflict exception', async () => {
       const game = new Game();
       game.followerList = [];
-      game.id = "1";
+      game.id = '1';
       const user = new User();
-      user.id = "1";
+      user.id = '1';
       const req = {
-        user: user
-      }; 
+        user: user,
+      };
       const gameSpy = jest
         .spyOn(gameRepository, 'findGameByIdWithFollowerList')
         .mockResolvedValue(game);
-      await expect(gameController.unfollowGame(req as any, game.id)).rejects.toStrictEqual(
-        new ConflictException("The Game is not followed!")
-        );   
+      await expect(
+        gameController.unfollowGame(req as any, game.id),
+      ).rejects.toStrictEqual(
+        new ConflictException('The Game is not followed!'),
+      );
       expect(gameSpy).toHaveBeenCalledWith(game.id);
-
     });
     it('should throw Not Found Exception', async () => {
       const game = new Game();
       game.followerList = [];
-      game.id = "1";
+      game.id = '1';
       const user = new User();
-      user.id = "1";
+      user.id = '1';
       const req = {
-        user: user
-      }; 
+        user: user,
+      };
       game.followerList.push(user);
       const gameSpy = jest
         .spyOn(gameRepository, 'findGameByIdWithFollowerList')
         .mockResolvedValue(undefined);
-      await expect(gameController.unfollowGame(req as any, game.id)).rejects.toStrictEqual(
-        new NotFoundException("Game Not Found!")
-      );
+      await expect(
+        gameController.unfollowGame(req as any, game.id),
+      ).rejects.toStrictEqual(new NotFoundException('Game Not Found!'));
       expect(gameSpy).toHaveBeenCalledWith(game.id);
     });
   });
