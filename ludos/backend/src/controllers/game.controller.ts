35b22384e1b6ad95efede0ee1e194997ae1357b7
Melih Gezer <melih_gezer211@hotmail.com>
2023-10-30 21:46:07 +0300

    finish game get flow

--- ludos/backend/src/controllers/game.controller.ts
+++ ludos/backend/src/controllers/game.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, HttpCode, NotFoundException, Param, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, NotFoundException, Param, Post } from '@nestjs/common';
 import {
   ApiBadRequestResponse,
   ApiConflictResponse,
