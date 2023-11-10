662616ad843c9d211696eecfa0b4a7249f6c003a
yunusemrealtug <yunusemrealtug64@gmail.com>
2023-11-10 13:59:44 +0300

    formatted

--- ludos/frontend/src/components/ListObject.js
+++ ludos/frontend/src/components/ListObject.js
@@ -1,26 +1,16 @@
 import React from "react";
-import {
-  
-  Typography,
-  Grid,
-  
-} from "@mui/material";
+import { Typography, Grid } from "@mui/material";
 
-function ListObject (description) {
-
+function ListObject(description) {
   return (
-    <Grid style={{width:"100%"}}>
-      {description.data.map((game, index1) => (
-              <Typography
-                variant="caption"
-                component="div"
-                key={index1}
-              >
-                {game}
-              </Typography>
-            ))}
+    <Grid style={{ width: "100%" }}>
+      {description.data.map((info, index1) => (
+        <Typography variant="caption" component="div" key={index1}>
+          {info}
+        </Typography>
+      ))}
     </Grid>
-    );
+  );
 }
 
 export default ListObject;
