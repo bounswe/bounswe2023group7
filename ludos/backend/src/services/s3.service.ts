eca321b16fef03785258b3f832b8326bbc8234a2
Hakan Karakuş <68596831+Hakkush-07@users.noreply.github.com>
2023-11-10 21:18:37 +0300

    start writing photo upload download functions and set aws params

---
+++ ludos/backend/src/services/s3.service.ts
@@ -0,0 +1,43 @@
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { S3 } from 'aws-sdk';
+
+@Injectable()
+export class S3Service {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  public initializeS3(): S3 {
+    let s3 = new S3({
+      region: this.configService.get<string>('AWS_BUCKET_REGION'),
+      accessKeyId: this.configService.get<string>('AWS_ACCESS_KEY'),
+      secretAccessKey: this.configService.get<string>('AWS_SECRET_KEY'),
+    });
+    return s3;
+  }
+
+  public async uploadPhoto(file: File): Promise<S3.ManagedUpload.SendData> {
+    let s3 = this.initializeS3();
+    let fileStream = file;
+    let uploadParams = {
+      Bucket: this.configService.get<string>('AWS_BUCKET_NAME'),
+      Body: fileStream,
+      Key: file.filename,
+    }
+    return s3.upload(uploadParams).promise();
+  }
+
+  public async downloadPhoto(fileKey: string) {
+    let downloadParams = {
+      Bucket: this.configService.get<string>('AWS_BUCKET_NAME'),
+      Key: fileKey,
+    }
+  }
+}
