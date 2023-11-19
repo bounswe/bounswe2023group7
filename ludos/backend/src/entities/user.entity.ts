d82c918b1fd84b4def522c92632081a8c4bac8b5
Melih Gezer <melih_gezer211@hotmail.com>
2023-11-19 10:58:59 +0300

    entity creation

--- ludos/backend/src/entities/user.entity.ts
+++ ludos/backend/src/entities/user.entity.ts
@@ -4,12 +4,14 @@ import {
   PrimaryGeneratedColumn,
   BeforeInsert,
   BeforeUpdate,
+  OneToMany,
   Index,
   ManyToMany,
 } from 'typeorm';
 import * as bcrypt from 'bcrypt';
 import { Game } from './game.entity';
 import { UserType } from '../enums/user-type.enum';
+import { Review } from './review.entity';
 @Entity('users')
 export class User {
   @PrimaryGeneratedColumn('uuid')
@@ -45,6 +47,9 @@ export class User {
 
   @Column({ nullable: true })
   steamUrl: string;
+
+  @OneToMany(() => Review, (review) => review.user)
+  reviews: Review[];
 
   @BeforeInsert()
   @BeforeUpdate()
