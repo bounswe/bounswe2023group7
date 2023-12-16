92b134f1ef0cd4d0e8052e59f0972f58cc70bd5e
yunusemrealtug <yunusemrealtug64@gmail.com>
2023-12-17 00:53:01 +0300

    create group instructed

--- ludos/frontend/src/App.js
+++ ludos/frontend/src/App.js
@@ -20,6 +20,7 @@ import EntityPage from "./pages/EntityPage.js";
 import axios from "axios";
 import SampleThreadPage from "./pages/SampleThreadPage.js";
 import CreateEntityPage from "./pages/CreateEntityPage.js";
+import CreateGroupPage from "./pages/CreateGroupPage.js";
 
 function App() {
   const [games, setGames] = useState([]);
@@ -495,6 +496,14 @@ function App() {
             element={
               <Layout>
                 <CreateEntityPage />
+              </Layout>
+            }
+          />
+          <Route
+            path="/create-group"
+            element={
+              <Layout>
+                <CreateGroupPage />
               </Layout>
             }
           />
