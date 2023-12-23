7241cbedd962cb9666a9f60e9e49d48570ffd72f
Elif Kizilkaya <69367633+elifkizilky@users.noreply.github.com>
2023-12-23 15:53:13 +0000

    added annotation to gamebio

--- ludos/frontend/src/pages/GamePage.js
+++ ludos/frontend/src/pages/GamePage.js
@@ -1,4 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
+import { Recogito } from "@recogito/recogito-js";
+import "@recogito/recogito-js/dist/recogito.min.css";
 import {
   Container,
   Grid,
@@ -20,6 +22,7 @@ import RelatedGames from "../components/RelatedGamesTab.js";
 import EntityTab from "../components/EntityTab.js";
 import GameForum from "../components/GameForums.js";
 import { useNavigate } from "react-router-dom";
+import { FaIgloo } from "react-icons/fa";
 
 function GamePage(id) {
   const navigate = useNavigate();
@@ -223,6 +226,120 @@ function GamePage(id) {
     alignItems: "center",
     flexDirection: "column",
     display: "flex",
+  };
+
+  useEffect(() => {
+    // Ensure gameBio is loaded before initializing Recogito
+    if (game && game.gameBio) {
+      const annotator = new Recogito({
+        content: "game-bio",
+      });
+
+      fetchAnnotations();
+
+      return () => annotator.destroy();
+    }
+  }, [game]); // Depend on the 'game' state
+
+  const annotatorRef = useRef(null);
+
+  useEffect(() => {
+    if (game && game.gameBio) {
+      annotatorRef.current = new Recogito({
+        content: "game-bio",
+        // Other initialization...
+      });
+
+      fetchAnnotations();
+      annotatorRef.current.on("createAnnotation", handleCreateAnnotation);
+
+      return () => annotatorRef.current.destroy();
+    }
+  }, []);
+
+  const handleCreateAnnotation = async (annotation) => {
+    // Prepare your API request body
+    const requestBody = {
+      "@context": "http://www.w3.org/ns/anno.jsonld",
+      type: "Annotation",
+      body: annotation.body[0].value, // You might need to format this according to your backend expectations
+      target: {
+        source: window.location.href,
+        selector: {
+          start: annotation.target.selector[1].start, // Starting character index
+          end: annotation.target.selector[1].end, // Ending character index
+        },
+      },
+    };
+
+    // Make the API call to your server
+    try {
+      const response = await axios.post(
+        `http://${process.env.REACT_APP_API_URL}/annotation/gamebio/${id.gameId}`,
+        requestBody,
+      );
+      console.log("Annotation saved:", response.data);
+    } catch (error) {
+      console.error("Error saving annotation:", error);
+    }
+  };
+
+  // ... existing useEffect code for other functionalities
+
+  // Function to fetch annotations
+  const fetchAnnotations = async () => {
+    try {
+      const response = await axios.get(
+        `http://${process.env.REACT_APP_API_URL}/annotation/gamebio/${id.gameId}`,
+        {
+          headers: {
+            Authorization: "Bearer " + localStorage.getItem("accessToken"),
+          },
+        },
+      );
+
+      if (response.data) {
+        displayAnnotations(response.data);
+      }
+    } catch (error) {
+      console.error("Error fetching annotations:", error);
+    }
+  };
+
+  const displayAnnotations = (gameBioAnnotations) => {
+    // Assuming 'annotations' is an array of annotation objects
+    // and you have an instance of Recogito called 'annotator'
+    const annotator = new Recogito({
+      content: "game-bio", // ID of the element that contains the text
+    });
+    gameBioAnnotations.forEach((annotation) => {
+      console.log("annotator annotation", annotation);
+      annotator.addAnnotation({
+        "@context": "http://www.w3.org/ns/anno.jsonld",
+        type: "Annotation",
+        id: annotation.id,
+        body: [
+          {
+            type: "TextualBody",
+            value: annotation.body,
+            purpose: "commenting",
+          },
+        ],
+        target: {
+          selector: [
+            {
+              type: "TextQuoteSelector",
+              exact: annotation.body,
+            },
+            {
+              type: "TextPositionSelector",
+              start: annotation.target.selector.start,
+              end: annotation.target.selector.end,
+            },
+          ],
+        },
+      });
+    });
   };
 
   const handleFollowClick = () => {
@@ -542,7 +659,8 @@ function GamePage(id) {
         <Grid item xs={12} sm={4} md={4} lg={4} style={{ marginLeft: "1%" }}>
           <Grid item xs={12} sm={12} md={12} lg={12} style={bioBoxStyle}>
             <Typography
-              component="legend"
+              component="div"
+              id="game-bio"
               style={{ fontFamily: "Trebuchet MS, sans-serif" }}
             >
               {game.gameBio}
