25e7380e4dfb7b536181d538233e1028b300a6f5
omersafakbebek <omersafakbebek@gmail.com>
2023-12-25 01:38:34 +0300

    refactor (BE)

--- ludos/backend/src/services/config/typeorm-config.service.ts
+++ ludos/backend/src/services/config/typeorm-config.service.ts
@@ -24,12 +24,12 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory {
       password: this.configService.get<string>('DB_PASSWORD'),
       port: this.configService.get<number>('DB_PORT'),
       database: this.configService.get<string>('DB_NAME'),
-      ssl: true,
-      extra: {
-        ssl: {
-          rejectUnauthorized: false,
-        },
-      },      
+      // ssl: true,
+      // extra: {
+      //   ssl: {
+      //     rejectUnauthorized: false,
+      //   },
+      // },      
       entities: [
         User,
         ResetPassword,
