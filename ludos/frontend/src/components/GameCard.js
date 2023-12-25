525fb763b1a955e471963581c290c5bfb336d3d7
yunusemrealtug <yunusemrealtug64@gmail.com>
2023-12-25 12:39:30 +0300

    added relatedgameslink

--- ludos/frontend/src/components/GameCard.js
+++ ludos/frontend/src/components/GameCard.js
@@ -1,168 +1,209 @@
-import { React, useEffect, useState } from 'react';
-import { Card, CardContent, Typography, Chip, Grid, Box } from '@mui/material';
-import StarRateRoundedIcon from '@mui/icons-material/StarRateRounded';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import { React, useEffect, useState } from "react";
+import { Card, CardContent, Typography, Chip, Grid, Box } from "@mui/material";
+import StarRateRoundedIcon from "@mui/icons-material/StarRateRounded";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const convertToSlug = (text) => {
-    return text
-        ?.toString()
-        .toLowerCase()
-        .trim()
-        .replace(/[\s_]/g, "-") // Replace spaces or underscores with dashes
-        .replace(/[^\w-]+/g, "") // Remove non-word characters except dashes
-        .replace(/--+/g, "-"); // Replace multiple dashes with single dash
-}
+  return text
+    ?.toString()
+    .toLowerCase()
+    .trim()
+    .replace(/[\s_]/g, "-") // Replace spaces or underscores with dashes
+    .replace(/[^\w-]+/g, "") // Remove non-word characters except dashes
+    .replace(/--+/g, "-"); // Replace multiple dashes with single dash
+};
 
 export default function GameCard({ game }) {
-    const [gameTitle, setGameTitle] = useState("");
+  const [gameTitle, setGameTitle] = useState("");
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleCardClick = (title) => {
-        setGameTitle(convertToSlug(title));
-        console.log(gameTitle);
-        navigate(`/game/${convertToSlug(title)}`);
-        window.location.reload(false);
-    }
+  const handleCardClick = (title) => {
+    setGameTitle(convertToSlug(title));
+    console.log(gameTitle);
+    navigate(`/game/${convertToSlug(title)}`);
+    window.location.reload(false);
+  };
 
-    const [averageRating, setAverageRating] = useState(0);
-    const [tags, setTags] = useState([]);
+  const [averageRating, setAverageRating] = useState(0);
+  const [tags, setTags] = useState([]);
 
-    const axiosInstance = axios.create({
-        baseURL: `http://${process.env.REACT_APP_API_URL}`,
-        headers: {
-            Authorization: "Bearer " + localStorage.getItem("accessToken"),
-        },
-    });
+  const axiosInstance = axios.create({
+    baseURL: `http://${process.env.REACT_APP_API_URL}`,
+    headers: {
+      Authorization: "Bearer " + localStorage.getItem("accessToken"),
+    },
+  });
 
-    const formatDate = (isoString) => {
-        const date = new Date(isoString);
-        return new Intl.DateTimeFormat('en-GB', {
-            day: 'numeric',
-            month: 'long',
-            year: 'numeric',
-        }).format(date);
-    };
+  const formatDate = (isoString) => {
+    const date = new Date(isoString);
+    return new Intl.DateTimeFormat("en-GB", {
+      day: "numeric",
+      month: "long",
+      year: "numeric",
+    }).format(date);
+  };
 
-    useEffect(() => {
-        axiosInstance.get(`/game/${game.id}`)
-            .then((response) => {
-                if (response.data.averageRating) {
-                    setAverageRating(response.data.averageRating.toFixed(1));
-                    setTags(response.data.tags);
-                }
-            })
-            .catch((error) => {
-                console.log(error);
-            })
-    }, [])
+  useEffect(() => {
+    axiosInstance
+      .get(`/game/${game.id}`)
+      .then((response) => {
+        if (response.data.averageRating) {
+          setAverageRating(response.data.averageRating.toFixed(1));
+          setTags(response.data.tags);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
-    const replaceImage = (error) => {
-        error.target.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/Video-Game-Controller-Icon-D-Edit.svg/2048px-Video-Game-Controller-Icon-D-Edit.svg.png";
-    }
+  const replaceImage = (error) => {
+    error.target.src =
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/Video-Game-Controller-Icon-D-Edit.svg/2048px-Video-Game-Controller-Icon-D-Edit.svg.png";
+  };
 
-    return (
-        <Card
-            xs={6}
+  return (
+    <Card
+      xs={6}
+      sx={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: 2,
+        borderRadius: "20px",
+        backgroundColor: "#171738",
+      }}
+    >
+      <Grid
+        container
+        spacing={1}
+        sx={{ display: "flex", flexDirection: "column" }}
+      >
+        <Grid
+          item
+          xs={12}
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+            alignItems: "center",
+            color: "black",
+          }}
+        >
+          <Box
+            onClick={() => handleCardClick(game.title)}
             sx={{
-                display: 'flex',
-                justifyContent: 'space-between',
-                alignItems: 'center',
-                padding: 2,
-                borderRadius: '20px',
-                backgroundColor: '#171738',
+              backgroundColor: "#B4CDED",
+              padding: "10px 100px",
+              borderRadius: "20px",
+              cursor: "pointer", // İmleci işaretçiye dönüştürür
+              "&:hover": {
+                backgroundColor: "#A1C4E4", // Hover durumunda arka plan rengi
+              },
+            }}
+          >
+            <Typography
+              variant="h6"
+              component="div"
+              style={{
+                fontWeight: "bold",
+              }}
+            >
+              {game.title}
+            </Typography>
+            <Typography
+              style={{
+                fontStyle: "italic",
+              }}
+            >
+              {game.developer}
+            </Typography>
+            <Typography variant="body2">
+              Release Date: {formatDate(game.releaseDate)}
+            </Typography>
+          </Box>
+        </Grid>
 
-            }}>
-            <Grid container spacing={1} sx={{ display: 'flex', flexDirection: 'column' }}>
-                <Grid item xs={12} sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: 'space-between',
-                    alignItems: 'center',
-                    color: 'black',
-                }}>
-                    <Box
-                        onClick={() => handleCardClick(game.title)}
-                        sx={{
-                            backgroundColor: '#B4CDED',
-                            padding: '10px 100px',
-                            borderRadius: '20px',
-                            cursor: 'pointer', // İmleci işaretçiye dönüştürür
-                            '&:hover': {
-                                backgroundColor: '#A1C4E4', // Hover durumunda arka plan rengi
-                            },
-                        }}>
-                        <Typography variant="h6" component="div" style={{
-                            fontWeight: 'bold',
-                        }}>
-                            {game.title}
-                        </Typography>
-                        <Typography style={{
-                            fontStyle: 'italic',
-                        }}>
-                            {game.developer}
-                        </Typography>
-                        <Typography variant="body2">
-                            Release Date: {formatDate(game.releaseDate)}
-                        </Typography>
-                    </Box>
-                </Grid>
-
-                <Grid item xs={12} sm={6} sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-                    <Box sx={{ marginRight: '16px', borderRadius: '20px' }}>
-                        <img
-                            src={game.coverLink}
-                            alt={game.title}
-                            style={{ height: 150, width: 150, borderRadius: '20px' }}
-                            onError={replaceImage}
-                        />
-                    </Box>
-                    <Box sx={{
-                        backgroundColor: '#7180B9',
-                        padding: '20px',
-                        borderRadius: '30px',
-                        height: '150px',
-                        display: 'flex',
-                        alignItems: 'center',
-                        justifyContent: 'center',
-
-                    }}>
-                        <Typography sx={{ width: '500px', color: 'white' }}>
-                            {game.gameBio.slice(0, 300)}...
-                        </Typography>
-                    </Box>
-                </Grid>
-                <Grid item xs={12} sx={{
-                    display: 'flex',
-                    flexDirection: 'row',
-                    alignItems: 'center',
-                    justifyContent: 'space-between',
-                }}>
-                    <div>
-                        {tags.map((tag, index) => (
-                            <Chip
-                                key={index}
-                                label={tag}
-                                color="primary"
-                                style={{ marginRight: '4px', backgroundColor: '#F75C03', fontWeight: 'bold' }}
-                            />
-                        ))}
-                    </div>
-                    <Typography variant="h5" component="div" sx={{
-                        color: '#003049',
-                        backgroundColor: '#63B0CD',
-                        borderRadius: '20px',
-                        padding: '12px',
-                        display: 'flex',
-                        alignItems: 'center',
-                    }}>
-                        {averageRating}
-                        <StarRateRoundedIcon sx={{ marginLeft: '5px', color: '#F77F00', width: '35px', height: '35px' }} />
-                    </Typography>
-                </Grid>
-            </Grid>
-        </Card >
-    );
+        <Grid
+          item
+          xs={12}
+          sm={6}
+          sx={{ display: "flex", flexDirection: "row", alignItems: "center" }}
+        >
+          <Box sx={{ marginRight: "16px", borderRadius: "20px" }}>
+            <img
+              src={game.coverLink}
+              alt={game.title}
+              style={{ height: 150, width: 150, borderRadius: "20px" }}
+              onError={replaceImage}
+            />
+          </Box>
+          <Box
+            sx={{
+              backgroundColor: "#7180B9",
+              padding: "20px",
+              borderRadius: "30px",
+              height: "150px",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Typography sx={{ width: "500px", color: "white" }}>
+              {game.gameBio.slice(0, 300)}...
+            </Typography>
+          </Box>
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <div>
+            {tags.map((tag, index) => (
+              <Chip
+                key={index}
+                label={tag}
+                color="primary"
+                style={{
+                  marginRight: "4px",
+                  backgroundColor: "#F75C03",
+                  fontWeight: "bold",
+                }}
+              />
+            ))}
+          </div>
+          <Typography
+            variant="h5"
+            component="div"
+            sx={{
+              color: "#003049",
+              backgroundColor: "#63B0CD",
+              borderRadius: "20px",
+              padding: "12px",
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            {averageRating}
+            <StarRateRoundedIcon
+              sx={{
+                marginLeft: "5px",
+                color: "#F77F00",
+                width: "35px",
+                height: "35px",
+              }}
+            />
+          </Typography>
+        </Grid>
+      </Grid>
+    </Card>
+  );
 }
-
--- ludos/frontend/src/components/RelatedGamesTab.js
+++ ludos/frontend/src/components/RelatedGamesTab.js
@@ -1,11 +1,21 @@
 import React, { useEffect } from "react";
-import { Typography, Grid, Box } from "@mui/material";
+import { Typography, Grid, Box, Link } from "@mui/material";
 import axios from "axios";
 import { useState } from "react";
 import GameCard from "./GameCard";
 
 function Description(data) {
   const [suggestedGames, setSuggestedGames] = useState([]);
+
+  const convertToSlug = (text) => {
+    return text
+      ?.toString()
+      .toLowerCase()
+      .trim()
+      .replace(/[\s_]/g, "-") // Replace spaces or underscores with dashes
+      .replace(/[^\w-]+/g, "") // Remove non-word characters except dashes
+      .replace(/--+/g, "-"); // Replace multiple dashes with single dash
+  };
 
   const axiosInstance = axios.create({
     baseURL: `http://${process.env.REACT_APP_API_URL}`,
@@ -15,14 +25,11 @@ function Description(data) {
   });
 
   useEffect(() => {
-    axiosInstance.get(`/game/${data.gameId}/related`)
-      .then((response) => {
-        setSuggestedGames(response.data.slice(0, 5));
-        console.log(response.data)
-      })
-
+    axiosInstance.get(`/game/${data.gameId}/related`).then((response) => {
+      setSuggestedGames(response.data.slice(0, 5));
+      console.log(response.data);
+    });
   }, []);
-
 
   const headerStyle = {
     marginBottom: "8px",
@@ -33,9 +40,16 @@ function Description(data) {
   };
 
   return (
-    <Grid container xs={8} style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
-
-      <Grid item xs={12} >
+    <Grid
+      container
+      xs={8}
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+      }}
+    >
+      <Grid item xs={12}>
         <Typography variant="h5" color="gray" align="left" style={headerStyle}>
           PREDECCESORS
         </Typography>
@@ -43,20 +57,24 @@ function Description(data) {
           variant="body1"
           color="white"
           align="left"
-          style={{ marginBottom: "8px", fontFamily: "Trebuchet MS, sans-serif" }}
+          style={{
+            marginBottom: "8px",
+            fontFamily: "Trebuchet MS, sans-serif",
+          }}
         >
           {data.predecessors.map((predecessor, index1) => (
-            <Typography
-              variant="body1"
-              color="white"
-              align="center"
-              style={{
-                fontFamily: "Trebuchet MS, sans-serif",
-              }}
-              key={index1}
-            >
-              {predecessor}
-            </Typography>
+            <Link to={`/game/${convertToSlug(predecessor)}`} key={index1}>
+              <Typography
+                variant="body1"
+                color="white"
+                align="center"
+                style={{
+                  fontFamily: "Trebuchet MS, sans-serif",
+                }}
+              >
+                {predecessor}
+              </Typography>
+            </Link>
           ))}
         </Typography>
       </Grid>
@@ -69,7 +87,10 @@ function Description(data) {
           variant="body1"
           color="white"
           align="left"
-          style={{ marginBottom: "8px", fontFamily: "Trebuchet MS, sans-serif" }}
+          style={{
+            marginBottom: "8px",
+            fontFamily: "Trebuchet MS, sans-serif",
+          }}
         >
           {data.successors.map((successor, index1) => (
             <Typography
@@ -89,18 +110,22 @@ function Description(data) {
 
       <Grid item xs={12}>
         <Box xs={4}>
-          <Typography variant="h5" color="gray" align="left" style={headerStyle}>
+          <Typography
+            variant="h5"
+            color="gray"
+            align="left"
+            style={headerStyle}
+          >
             SUGGESTED GAMES
           </Typography>
           {suggestedGames.map((game, index) => (
-            <div key={index} style={{ marginTop: '10px' }}>
+            <div key={index} style={{ marginTop: "10px" }}>
               <GameCard game={game} />
             </div>
           ))}
         </Box>
       </Grid>
-
-    </Grid >
+    </Grid>
   );
 }
 
